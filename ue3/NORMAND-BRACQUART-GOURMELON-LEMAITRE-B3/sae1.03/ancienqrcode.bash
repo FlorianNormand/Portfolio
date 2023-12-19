23b9862f39e2d0d70a8e85914958c75820dad902
FlorianNormand <122635478+FlorianNormand@users.noreply.github.com>
2023-12-19 11:49:17 +0100

    Delete ue3 directory

--- ue3/NORMAND-BRACQUART-GOURMELON-LEMAITRE-B3/sae1.03/ancienqrcode.bash
+++
@@ -1,2 +0,0 @@
-docker container run -ti -v $2:/work bigpapoo/sae103-qrcode qrencode -o code.png "https://bigbrain.biz/$iso"
-
--- ue3/NORMAND-BRACQUART-GOURMELON-LEMAITRE-B3/sae1.03/final.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# TODO automatisé, récupérer les données
-# php ./script2.php
-# ./qrcode.bash
-# ./scriptAvatar.bash
-# php ./scriptHtmlnew.php
-# contenairisé
